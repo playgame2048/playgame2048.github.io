eed93bdbc4fb01955b79f402e6b2cfb7f66d3bc0
play2048 <mehdimehdi10782@gmail.com>
2026-02-02 17:21:18 +0100

    Update main.js

--- snake/main.js
+++ snake/main.js
@@ -34,16 +34,48 @@ document.addEventListener("keydown", e=>{
   if(e.key==="ArrowRight" && direction!=="LEFT") direction="RIGHT";
 });
 
+ctx.strokeStyle="rgba(255,255,255,0.05)";
+for(let i=0;i<canvas.width;i+=box){
+  ctx.beginPath();
+  ctx.moveTo(i,0);
+  ctx.lineTo(i,canvas.height);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(0,i);
+  ctx.lineTo(canvas.width,i);
+  ctx.stroke();
+}
+
 function draw(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
 
   snake.forEach((s,i)=>{
-    ctx.fillStyle = i===0 ? "lime" : "green";
-    ctx.fillRect(s.x,s.y,box,box);
+    ctx.fillStyle = i===0 ? "#22c55e" : "#16a34a";
+    ctx.beginPath();
+ctx.arc(
+  s.x + box/2,
+  s.y + box/2,
+  box/2.2,
+  0,
+  Math.PI * 2
+);
+ctx.fill();
   });
 
-  ctx.fillStyle="red";
-  ctx.fillRect(food.x,food.y,box,box);
+  ctx.beginPath();
+ctx.arc(
+  food.x + box/2,
+  food.y + box/2,
+  box/2.3,
+  0,
+  Math.PI*2
+);
+ctx.fillStyle = "#ef4444";
+ctx.shadowColor = "#ef4444";
+ctx.shadowBlur = 15;
+ctx.fill();
+ctx.shadowBlur = 0;
 
   let headX = snake[0].x;
   let headY = snake[0].y;
